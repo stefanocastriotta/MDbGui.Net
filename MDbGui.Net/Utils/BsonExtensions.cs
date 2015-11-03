219946dc5ade8365e31893e9f8cb2fcf8131c7bc
stefano.castriotta <stefano.castriotta>
2015-11-03 22:20:43 +0100

    Fixed json serialization bug

--- MDbGui.Net/Utils/BsonExtensions.cs
+++ MDbGui.Net/Utils/BsonExtensions.cs
@@ -12,7 +12,7 @@ namespace MDbGui.Net.Utils
         public static string ToJson(this BsonValue value, JsonWriterSettingsExtended settings = null)
         {
             StringBuilder sb = new StringBuilder();
-            var serializer = MongoDB.Bson.Serialization.BsonSerializer.LookupSerializer(typeof(BsonDocument));
+            var serializer = MongoDB.Bson.Serialization.BsonSerializer.LookupSerializer(typeof(BsonValue));
             MongoDB.Bson.Serialization.BsonSerializationArgs args = default(MongoDB.Bson.Serialization.BsonSerializationArgs);
 
             using (var stringWriter = new System.IO.StringWriter())
@@ -20,7 +20,7 @@ namespace MDbGui.Net.Utils
                 using (var bsonWriter = new LocalDateTimeJsonWriter(stringWriter, settings))
                 {
                     var context = MongoDB.Bson.Serialization.BsonSerializationContext.CreateRoot(bsonWriter, null);
-                    args.NominalType = typeof(BsonDocument);
+                    args.NominalType = typeof(BsonValue);
                     serializer.Serialize(context, args, value);
                 }
                 sb.Append(stringWriter.ToString());
